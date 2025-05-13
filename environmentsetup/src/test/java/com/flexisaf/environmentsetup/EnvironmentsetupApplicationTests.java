a2d9ba8715e8ea432d070ef1e90126db5b71ed51
Anakhe Ajayi <id3velope@gmail.com>
2025-05-13 17:27:17 +0100

    Flexisaf  backend deliverable - Spring Boot setup

---
+++ environmentsetup/src/test/java/com/flexisaf/environmentsetup/EnvironmentsetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexisaf.environmentsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentsetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
